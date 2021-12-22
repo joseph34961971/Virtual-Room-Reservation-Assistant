1b065fa4913dd81a43fdb045f7dc1dfa27898152
Chef Wu <j0978577077j@gmail.com>
2021-12-22 19:28:52 +0800

    change ARCH

---
+++ src/views/DateSelect.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="dateselect">
+    <h1>This is an about page</h1>
+    <el-date-picker
+      v-model="testDate"
+      type="date"
+      placeholder="Pick a day">
+    </el-date-picker>
+
+    <el-button
+      type="success"
+      @click="test"
+    >
+      確認!
+    </el-button>
+    <br>
+    <router-link :to="{name:'Home'}">BackHome</router-link>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Watch } from 'vue-property-decorator'
+import { component } from 'vue/types/umd';
+
+@Component({
+  components: {
+    test
+  }
+})
+
+export default class test extends Vue {
+  private testDate = ''
+
+  private test() {
+    console.log(this.testDate)
+  }
+
+}
+</script>
+
+<style>
---
+++ src/views/Login.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="login">
+    <h1>login page</h1>
+    <router-link :to="{name:'Home'}">BackHome</router-link>
+  </div>
+</template>
