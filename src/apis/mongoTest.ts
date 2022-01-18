2aa3acad94b7a80794c20b461b3f08be427fc598
Hao-Wen Chiang <jeff99998888@yahoo.com>
2022-01-18 21:04:59 +0800

    calendar 相關function 增加參數calID

--- src/apis/mongoTest.ts
+++ src/apis/mongoTest.ts
@@ -1,8 +1,42 @@
-
-
+  import * as http from 'http';
+  
   export async function connectMongo()
   {
+
     const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    
+    const raw = {
+      "name": "thirdUser",
+      "emailId": "jeff99998888@yahoo.com",
+      "password": "lolol",
+      "gender":"male", 
+      "address": "000000000000000000"
+      };
+    
+    console.log(JSON.stringify(raw));
+
+    const requestOptions = {
+      method: 'POST',
+      headers: new Headers({'Content-Type':'application/json'}),
+      body: raw,
+      redirect: 'follow'
+    };
+    
+    const response = await fetch("http://localhost:4000/user", {
+      method: 'POST',
+      mode :'no-cors',
+      headers: {'Content-Type':'application/json',
+                'Host':'localhost:4000'},
+      body: JSON.stringify(raw),
+      redirect: 'follow'})
+      .then(res => res.json())
+      .then(result => console.log(result))
+      .catch(error => console.log('error', error));
+
+    console.log(response);
+
+    /*const myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
 
     const resource = {
@@ -24,7 +58,7 @@
       redirect: 'follow'
     }).then(response => response.text())
     .then(result => console.log(result))
-    .catch(error => console.log('error', error));
+    .catch(error => console.log('error', error));*/
     
     
     
