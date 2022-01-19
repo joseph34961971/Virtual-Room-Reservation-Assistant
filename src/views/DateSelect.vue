50a139b37ff9fbbd823122317e265378f8aa0ccb
孟廷 鍾 <joseph34961971@gmail.com>
2022-01-19 22:29:43 +0800

    會議時間驗證

--- src/views/DateSelect.vue
+++ src/views/DateSelect.vue
@@ -72,16 +72,21 @@
           <el-form-item
             label="會議時間"
           >
-            <el-time-select
-              placeholder="起始时间"
-              v-model="startTime"
-              :picker-options="{
-                start: '08:00',
-                step: '01:00',
-                end: '17:00'
-              }">
-            </el-time-select>
-            <el-time-select
+            <el-select
+              v-model="selectedTime"
+              placeholder="請選擇時間"
+              style="width: 200px"
+              class="filter-item"
+            >
+              <el-option
+                v-for="item in timeOptions"
+                :key="item.label"
+                :label="item.label"
+                :value="item.label"
+                :disabled="item.disabled"
+              />
+            </el-select>
+            <!-- <el-time-select
               placeholder="結束时间"
               v-model="endTime"
               :picker-options="{
@@ -90,7 +95,7 @@
                 end: '18:00',
                 minTime: startTime
               }">
-            </el-time-select>
+            </el-time-select> -->
           </el-form-item>
           <el-form-item
             label="參與者"
@@ -115,7 +120,7 @@
 
 <script lang="ts">
 import { Vue, Component, Watch, PropSync } from 'vue-property-decorator'
-import { listUpcomingEvents, handleClientLoad, insertEvents } from '@/apis/googleCal'
+import { listUpcomingEvents, handleClientLoad, insertEvents, sendMail } from '@/apis/googleCal'
 import { component } from 'vue/types/umd';
 import About from './views/About.vue';
 import { ServerHeartbeatFailedEvent } from 'mongodb';
@@ -138,6 +143,21 @@ export default class test extends Vue {
     { value: 'jo63b6s8sn678tcesnpge03a6k@group.calendar.google.com', label: 'Room5' }
   ]
 
+  private defaultTimeOptions = [
+    { label: '08:00 - 09:00', start: '08:00', end: '09:00', disabled: false},
+    { label: '09:00 - 10:00', start: '09:00', end: '10:00', disabled: false},
+    { label: '10:00 - 11:00', start: '10:00', end: '11:00', disabled: false},
+    { label: '11:00 - 12:00', start: '11:00', end: '12:00', disabled: false},
+    { label: '12:00 - 13:00', start: '12:00', end: '13:00', disabled: false},
+    { label: '13:00 - 14:00', start: '13:00', end: '14:00', disabled: false},
+    { label: '14:00 - 15:00', start: '14:00', end: '15:00', disabled: false},
+    { label: '15:00 - 16:00', start: '15:00', end: '16:00', disabled: false},
+    { label: '16:00 - 17:00', start: '16:00', end: '17:00', disabled: false},
+    { label: '17:00 - 18:00', start: '17:00', end: '18:00', disabled: false},
+  ]
+  private timeOptions = this.defaultTimeOptions
+  private selectedTime = ''
+
   private calendarID = ''
   private startTime = ''
   private endTime = ''
@@ -151,13 +171,49 @@ export default class test extends Vue {
   
   create() {
     handleClientLoad()
-    this.getList()
   }
 
-  private enterDate() {
+  private async enterDate() {
     //this.$router.push('Reservation')
+    //this.timeOptions = this.defaultTimeOptions
+
+    for (let i in this.timeOptions) {
+      this.timeOptions[i].disabled = false
+    }
+
     if (this.calendarID != '') {
-      this.getList()
+      
+      this.listLoading = true
+      const data = await listUpcomingEvents(this.calendarID)
+      if (data == 0) {
+        console.log('nothing')
+      }
+      else {
+        let c = 0
+        //console.log(typeof(data))
+        for (let i in data) {
+          let tempStartTime = data[i].start.dateTime
+          let tempEndTime = data[i].end.dateTime
+          tempStartTime = tempStartTime.split('T')
+          //console.log(tempStartTime[0])
+          //console.log(this.date)
+          if (tempStartTime[0] != this.date) {
+            continue
+          }
+          c = c + 1
+          tempStartTime = tempStartTime[1].split(':')[0]
+          console.log(tempStartTime)
+          for (let j in this.timeOptions) {
+            if (this.timeOptions[j].start == (tempStartTime + ":00")) {
+              this.timeOptions[j].disabled = true
+            }
+          }
+        }
+      //console.log(c)
+
+      }
+      this.listLoading = false
+      
       console.log(this.calendarID)
       console.log(this.date)
       this.reservationFormVisible = true
@@ -181,14 +237,21 @@ export default class test extends Vue {
       console.log('nothing')
     }
     else {
-      console.log(data)
+      console.log(typeof(data))
+      for (let i in data) {
+        console.log(data[i].start.dateTime)
+        
+      }
     }
-    console.log(data)
     this.listLoading = false
   }
 
   private handleReservation() {
     this.eventData.calendarID = this.calendarID
+    let tempTime = this.selectedTime.split(' - ')
+    this.startTime = tempTime[0]
+    this.endTime = tempTime[1]
+    console.log(this.selectedTime)
     this.eventData.startTime = this.date + "T" + this.startTime + ":00" + "+08:00"
     this.eventData.endTime = this.date + "T" + this.endTime + ":00" + "+08:00"
     console.log(this.eventData)
